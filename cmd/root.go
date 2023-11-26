24983548bc7c26f0a0e30fcda070d2d0b5dcfcc3
xm1k3 <73166077+xm1k3@users.noreply.github.com>
2023-11-26 12:03:51 +0100

    feat: init project

---
+++ cmd/root.go
@@ -0,0 +1,184 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	Host = "https://openapi.tuyaeu.com"
+)
+
+var (
+	Token string
+)
+
+type TokenResponse struct {
+	Result struct {
+		AccessToken  string `json:"access_token"`
+		ExpireTime   int    `json:"expire_time"`
+		RefreshToken string `json:"refresh_token"`
+		UID          string `json:"uid"`
+	} `json:"result"`
+	Success bool  `json:"success"`
+	T       int64 `json:"t"`
+}
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "tuya-brasov",
+	Short: "Tuya Brasov",
+	Long:  `Tuya Brasov`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) {
+	// },
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	rootCmd.PersistentFlags().StringP("clientid", "c", "", "clientId parameter")
+	rootCmd.PersistentFlags().StringP("secret", "s", "", "secret parameter")
+	rootCmd.PersistentFlags().StringP("deviceid", "d", "", "device parameter")
+
+	rootCmd.MarkPersistentFlagRequired("clientid")
+	rootCmd.MarkPersistentFlagRequired("secret")
+}
+
+func GetToken(clientid, secret, deviceid string) string {
+	method := "GET"
+	body := []byte(``)
+	req, _ := http.NewRequest(method, Host+"/v1.0/token?grant_type=1", bytes.NewReader(body))
+
+	buildHeader(clientid, secret, deviceid, req, body)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	defer resp.Body.Close()
+	bs, _ := ioutil.ReadAll(resp.Body)
+	ret := TokenResponse{}
+	json.Unmarshal(bs, &ret)
+	// fmt.Println(string(bs))
+
+	if v := ret.Result.AccessToken; v != "" {
+		Token = v
+	}
+	return string(bs)
+}
+
+func GetDevice(clientid, secret, deviceId string) string {
+	method := "GET"
+	body := []byte(``)
+	req, _ := http.NewRequest(method, Host+"/v1.0/devices/"+deviceId, bytes.NewReader(body))
+
+	buildHeader(clientid, secret, deviceId, req, body)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	defer resp.Body.Close()
+	bs, _ := ioutil.ReadAll(resp.Body)
+	// fmt.Println(string(bs))
+	return string(bs)
+}
+
+func buildHeader(clientid, secret, deviceid string, req *http.Request, body []byte) {
+	req.Header.Set("client_id", clientid)
+	req.Header.Set("sign_method", "HMAC-SHA256")
+
+	ts := fmt.Sprint(time.Now().UnixNano() / 1e6)
+	req.Header.Set("t", ts)
+
+	if Token != "" {
+		req.Header.Set("access_token", Token)
+	}
+
+	sign := buildSign(clientid, secret, deviceid, req, body, ts)
+	req.Header.Set("sign", sign)
+}
+
+func buildSign(clientid, secret, deviceid string, req *http.Request, body []byte, t string) string {
+	headers := getHeaderStr(req)
+	urlStr := getUrlStr(req)
+	contentSha256 := Sha256(body)
+	stringToSign := req.Method + "\n" + contentSha256 + "\n" + headers + "\n" + urlStr
+	signStr := clientid + Token + t + stringToSign
+	sign := strings.ToUpper(HmacSha256(signStr, secret))
+	return sign
+}
+
+func Sha256(data []byte) string {
+	sha256Contain := sha256.New()
+	sha256Contain.Write(data)
+	return hex.EncodeToString(sha256Contain.Sum(nil))
+}
+
+func getUrlStr(req *http.Request) string {
+	url := req.URL.Path
+	keys := make([]string, 0, 10)
+
+	query := req.URL.Query()
+	for key, _ := range query {
+		keys = append(keys, key)
+	}
+	if len(keys) > 0 {
+		url += "?"
+		sort.Strings(keys)
+		for _, keyName := range keys {
+			value := query.Get(keyName)
+			url += keyName + "=" + value + "&"
+		}
+	}
+
+	if url[len(url)-1] == '&' {
+		url = url[:len(url)-1]
+	}
+	return url
+}
+
+func getHeaderStr(req *http.Request) string {
+	signHeaderKeys := req.Header.Get("Signature-Headers")
+	if signHeaderKeys == "" {
+		return ""
+	}
+	keys := strings.Split(signHeaderKeys, ":")
+	headers := ""
+	for _, key := range keys {
+		headers += key + ":" + req.Header.Get(key) + "\n"
+	}
+	return headers
+}
+
+func HmacSha256(message string, secret string) string {
+	key := []byte(secret)
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(message))
+	sha := hex.EncodeToString(h.Sum(nil))
+	return sha
+}
